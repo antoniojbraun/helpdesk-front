24daa2c1194111730c58c1186126607f818dcbfd
Antonio J. Braun <antoniojbraun@gmail.com>
2024-07-04 09:25:01 -0300

    add redirect para /login assim que feito logout

--- app/ui/dashboard/sign-out-button.tsx
+++ app/ui/dashboard/sign-out-button.tsx
@@ -4,7 +4,7 @@ import { signOut } from "next-auth/react";
 import { poppinsRegular } from "../fonts";
 
 export async function signOutButton() {
-  await signOut();
+  await signOut({ callbackUrl: "/login", redirect: true });
 }
 
 export const SignOutButton = ({
@@ -13,17 +13,17 @@ export const SignOutButton = ({
   isSidebarOpen: boolean;
 }) => {
   return (
-      <button
-        className="flex items-center w-full space-x-[18px] py-[6px] px-[15px] hover:bg-[#2C88D9] hover:rounded-lg hover:bg-opacity-15 active:bg-opacity-20"
-        onClick={() => signOutButton()}>
-        <ArrowLeftStartOnRectangleIcon className="size-5 text-[#2C88D9]" />
+    <button
+      className="flex items-center w-full space-x-[18px] py-[6px] px-[15px] hover:bg-[#2C88D9] hover:rounded-lg hover:bg-opacity-15 active:bg-opacity-20"
+      onClick={() => signOutButton()}>
+      <ArrowLeftStartOnRectangleIcon className="size-5 text-[#2C88D9]" />
 
-        <p
-          className={`${
-            isSidebarOpen ? "md:hidden" : "hidden "
-          } md:block text-[#2C88D9] ${poppinsRegular.className} text-[14px]`}>
-          Sair
-        </p>
-      </button>
+      <p
+        className={`${
+          isSidebarOpen ? "md:hidden" : "hidden "
+        } md:block text-[#2C88D9] ${poppinsRegular.className} text-[14px]`}>
+        Sair
+      </p>
+    </button>
   );
 };
